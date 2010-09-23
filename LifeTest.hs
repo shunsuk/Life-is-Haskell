c1f853e16f7c0679ffab790f7666d74e3a3a7893
miyazaki <miyazaki@MacBook-Air.local>
2010-09-23 16:14:06 +0900

    added [Quit]

--- LifeTest.hs
+++ LifeTest.hs
@@ -1,12 +1,23 @@
 import LifeEngine
 
+type Pos = (Int, Int)
+
+goto :: Pos -> IO()
+goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
 next :: Cells -> IO ()
-next css  = do ln <- getLine
-               css' <- return $ nextGeneration css
-               mapM_ print css'
-               next css'
+next css  = do c <- getChar
+               goto (0, 0)
+               putStr "\ESC[2J"
+               if c `elem` "xXqQ\ESC"
+                  then putStrLn "Bye"
+                  else do css' <- return $ nextGeneration css
+                          mapM_ print css'
+                          next css'
 
 main :: IO ()
-main = do css <- initLife 10 10
+main = do putStr "\ESC[2J"
+          goto (0, 0)
+          css <- initLife 10 10
           mapM_ print css
           next css
